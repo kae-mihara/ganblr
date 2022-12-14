6f29010046720b9b13eb4ca1a95e7f8583f325be
kae zhou <kaemihara@outlook.com>
2022-12-15 01:22:49 +0800

    change version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except Exception as e:
 
 setup(
     name = "ganblr",
-    version = "0.1.0",
+    version = "0.1.1",
     keywords = ["ganblr", "tulip"],
     description = "Ganblr Toolbox",
     long_description = long_description,
